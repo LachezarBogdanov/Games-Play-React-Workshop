076d63fe91465906b7501e2aa2a0cea7e4e7ddb3
LachezarBogdanov <lachezarbogdanov22@gmail.com>
2025-03-14 15:32:27 +0200

    Added static pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,10 @@ import { Routes, Route } from 'react-router'
 
 import Header from './components/Header/Header'
 import Home from './components/Home/Home'
+import Login from './components/Login/Login'
+import Register from './components/Register/Register'
+import GameCatalog from './components/GameCatalog/GameCatalog'
+import GameCreate from './components/GameCreate/GameCreate'
 
 import './App.css'
 
@@ -14,6 +18,10 @@ function App() {
             <main id="main-content">
                 <Routes>
                     <Route index element={<Home />}/>
+                    <Route path="/login" element={<Login />}/>
+                    <Route path="/register" element={<Register />}/>
+                    <Route path="/games" element={<GameCatalog />}/>
+                    <Route path="/games/create" element={<GameCreate />}/>
                 </Routes>
                 
             </main>
