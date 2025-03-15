90a86dda102d18e3ea714d71c9a2e12373645050
LachezarBogdanov <lachezarbogdanov22@gmail.com>
2025-03-15 15:37:44 +0200

    List all games

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -4,10 +4,10 @@ const baseUrl = 'http://localhost:3030/jsonstore/games';
 
 export default {
     async getAll() {
-        const result = request.get(baseUrl);
-
+        const result = await request.get(baseUrl);
+        
         const games = Object.values(result);
-
+        
         return games;
     },
     create(gameData) {
